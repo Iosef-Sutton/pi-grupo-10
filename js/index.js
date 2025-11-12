ca1fbd13e16d08b7a25bebd67f2969316dead661
Iosef-Sutton <isuttondabbah@udesa.edu.ar>
2025-11-12 11:12:10 -0300

    Agregue archivos js, pero no edite casi nada

--- js/index.js
+++ js/index.js
@@ -42,3 +42,14 @@ fetch('https://dummyjson.com/products/category/womens-watches')
     //Aca termina la segunda seccion
 //Aca termina la home
 
+//Aca empieza el filtro de la barra buscadora
+const qs = location.search;
+const qso = new URLSearchParams(qs);
+const buscador = qso.get('Buscador');
+
+if(buscador == ""){
+    alert("Ingresa alguna palabra")
+} else if(buscador.length < 3){
+    alert("Igresa por lo menos 3 caracteres")
+}
+//Aca termina
---
+++ js/resultado_de_busqueda.js
@@ -0,0 +1,11 @@
+//Aca empieza el filtro de la barra buscadora
+const qs = location.search;
+const qso = new URLSearchParams(qs);
+const buscador = qso.get('Buscador');
+
+if(buscador == ""){
+    alert("Ingresa alguna palabra")
+} else if(buscador.length < 3){
+    alert("Igresa por lo menos 3 caracteres")
+}
+//Aca termina
