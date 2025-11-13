524f38a372d41feea10b1ddc4926d4796d572d16
cherreraposse-art <cherreraposse@udesa.edu.ar>
2025-11-13 18:49:06 -0300

    empece el login.js

---
+++ js/login.js
@@ -0,0 +1,26 @@
+let form = document.querySelector("#login-form");
+
+form.addEventListener("submit", function () {
+
+    let email = document.querySelector("#email").value;
+    let password = document.querySelector("#password").value;
+
+    if (email === "") {
+        alert("El email es obligatorio");
+        return false; // detiene el envío
+    }
+
+    if (password === "") {
+        alert("La contraseña es obligatoria");
+        return false;
+    }
+
+    if (password.length < 6) {
+        alert("La contraseña debe tener al menos 6 caracteres");
+        return false;
+    }
+
+    localStorage.setItem("userEmail", email);
+
+    return true; // permite que el formulario vaya a index.html
+});
