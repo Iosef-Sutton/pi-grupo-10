8f74d128ab7b6bead592a2588bc20d926c82ce43
cherreraposse-art <cherreraposse@udesa.edu.ar>
2025-11-13 18:58:27 -0300

    Agrego Parte B del login: saludo.js

---
+++ js/saludo.js
@@ -0,0 +1,8 @@
+//parte b de log in
+
+let userEmail = localStorage.getItem("userEmail");
+
+let saludo = document.querySelector("#saludo");
+let loginLink = document.querySelector("#login-link");
+let registerLink = document.querySelector("#register-link");
+let logoutLink = document.querySelector("#logout-link");
