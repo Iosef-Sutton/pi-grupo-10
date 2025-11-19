bedeac76ef2602ccc2821ca7dc963703e2db91b4
cherreraposse-art <cherreraposse@udesa.edu.ar>
2025-11-19 18:35:54 -0300

    cambios en logout

--- js/logout.js
+++ js/logout.js
@@ -1,19 +1,30 @@
 let logoutLink = document.querySelector(".logout-link");
-let saludo = document.querySelector(".saludo");
-let lista = document.querySelector(".lista");
 
-if (logoutLink && saludo && lista) {
+if (logoutLink) {
     logoutLink.addEventListener("click", function (e) {
     e.preventDefault(); 
         
     localStorage.removeItem("userEmail");  
 
-    saludo.innerText = ""; 
+    const saludoElemento = document.querySelector(".saludo");
+    const listaElemento = document.querySelector(".lista");
 
-    lista.innerHTML = `
+    if (saludoElemento) {
+    saludo.innerText = ""; 
+    }
+
+    if (listaElemento) {
+      listaElemento.innerHTML = `
         <li><a href="./index.html">Home</a></li>
         <li><a href="./login.html">Login</a></li>
         <li><a href="./register.html">Registro</a></li>
-        `;
-    });
+      `;
+    }
+  });
 }
+     
+     
+        
+        
+        
+
